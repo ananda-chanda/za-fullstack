2250f79caffe10686529148a94614f858e7df247
Ananda Chanda <anandachanda08@gmail.com>
2024-10-26 21:23:19 +0530

    v1

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,11 +1,11 @@
 // src/utils/api.js
 import axios from 'axios';
 
-const api = axios.create({
+const Api = axios.create({
   baseURL: 'http://localhost:5173/api', // Update this to match your backend URL
   headers: {
     'Content-Type': 'application/json',
   },            
 });
 
-export default api;   
+export default Api;   
